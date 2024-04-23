3bb4ef51970cc37ddca40519447d107aab33f466
zeros <luca.perini.illustrations@gmail.com>
2024-04-23 19:28:15 +0200

    migliorie

--- resources/views/chi-siamo.blade.php
+++ resources/views/chi-siamo.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Selfwork230424</title>
+    <title>Esercitazione 23/04/2024</title>
 </head>
 <body>
     <h1>{{$title}}</h1>
